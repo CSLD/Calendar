483b1ecd0e5ce1d1726797b206b1a3e0111a62f9
Jakub Balhar <balhar.jakub@gmail.com>
2015-06-21 23:07:26 +0200

    Issues to resolve.

--- src/main/java/net/balhar/calendar/frontend/package-info.java
+++ src/main/java/net/balhar/calendar/frontend/package-info.java
@@ -1,6 +1,6 @@
 /**
  * Issues to solve:
- * Configure default mapping based on the configuration.
- * Configure different RestApis with different providers.
+ * - Configure default mapping based on the configuration.
+ * - Configure different RestApis with different providers.
  */
 package net.balhar.calendar.frontend;
